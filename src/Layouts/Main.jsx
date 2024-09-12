1dbb2c764ffaa18fffb27635198dfb1875d1abc3
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-12 11:41:51 +0600

    footer navbar added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Sheared/Footer/Footer";
+import Navbar from "../Pages/Home/Sheared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
