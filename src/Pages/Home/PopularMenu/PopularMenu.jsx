28f2d1940073aaa9b490040c0a240b38873e8f70
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-12 21:37:16 +0600

    json file added

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,11 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <div>
+      <SectionTitle subHeading={"Popular Items"} heading={"From Our Menu"} />
+    </div>
+  );
+};
+
+export default PopularMenu;
