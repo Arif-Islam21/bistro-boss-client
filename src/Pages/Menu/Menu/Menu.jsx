158619e16a8f8b91dc347fe0dc9c76595f070cc1
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-13 14:44:07 +0600

    helmet implemented

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,12 @@
+import { Helmet } from "react-helmet-async";
+
 const Menu = () => {
   return (
     <div>
+      <Helmet>
+        <title>Bistro | Menu</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
       <h2> menu here</h2>
     </div>
   );
