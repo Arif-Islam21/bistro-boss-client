4a94020b589cfb0f26fd431ffa9980d4c0be2eed
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-13 10:44:59 +0600

    menu added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../Sheared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popularItems = data.filter((item) => item.category === "popular");
+        setMenu(popularItems);
+      });
+  }, []);
+
   return (
     <div>
       <SectionTitle subHeading={"Popular Items"} heading={"From Our Menu"} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 my-4">
+        {menu.map((item) => (
+          <MenuItem key={item._id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
