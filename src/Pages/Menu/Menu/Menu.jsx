be8f9007c0fb315c9839c052372f0064c7dd1c1f
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-13 15:09:27 +0600

    paralax done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Home/Sheared/Cover/Cover";
+import img from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -7,7 +10,12 @@ const Menu = () => {
         <title>Bistro | Menu</title>
         <link rel="canonical" href="https://www.tacobell.com/" />
       </Helmet>
-      <h2> menu here</h2>
+      <Cover img={img} title={"Our Menu"} />
+      <PopularMenu />
+      <Cover img={img} title={"Our Menu"} />
+      <PopularMenu />
+      <Cover img={img} title={"Our Menu"} />
+      <PopularMenu />
     </div>
   );
 };
