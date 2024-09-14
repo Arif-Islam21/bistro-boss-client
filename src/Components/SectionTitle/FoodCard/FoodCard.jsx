90c9fd55633a6159f7277d8954e112fd9f5155da
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-14 12:23:30 +0600

    implement pagination

--- src/Components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/Components/SectionTitle/FoodCard/FoodCard.jsx
@@ -11,8 +11,10 @@ const FoodCard = ({ item }) => {
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add TO Cart</button>
+        <div className="card-actions justify-center">
+          <button className="btn btn-outline bg-slate-100 border-orange-400 text-orange-400 border-0 border-b-4 mt-4">
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
