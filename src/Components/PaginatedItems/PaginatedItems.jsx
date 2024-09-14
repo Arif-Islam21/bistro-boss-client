4b8f3770506cea06174708c7a9bc1fe0452aaa9c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-14 15:17:14 +0600

    pagination done with react paginate

--- src/Components/PaginatedItems/PaginatedItems.jsx
+++ src/Components/PaginatedItems/PaginatedItems.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import ReactPaginate from "react-paginate";
+import FoodCard from "../SectionTitle/FoodCard/FoodCard";
 
-const PaginatedItems = ({ itemsPerPage, items, setShowItem }) => {
+const PaginatedItems = ({ itemsPerPage, items }) => {
   const [itemOffset, setItemOffset] = useState(0);
 
   const endOffset = itemOffset + itemsPerPage;
@@ -15,21 +16,40 @@ const PaginatedItems = ({ itemsPerPage, items, setShowItem }) => {
     console.log(
       `User requested page number ${event.selected}, which is offset ${newOffset}`
     );
+    setItemOffset(newOffset);
   };
 
   return (
     <div>
       {/* <Items currentItems={currentItems} /> */}
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-        className="flex gap-4 items-center"
-      />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+        {currentItems.map((item) => (
+          <FoodCard key={item._id} item={item} />
+        ))}
+      </div>
+      <div className="flex justify-center my-8">
+        <ReactPaginate
+          breakLabel={
+            <span className="px-4 py-2 btn border rounded-lg">...</span>
+          }
+          nextLabel={
+            <span className="px-4 py-2 rounded-lg btn btn-outline border-0 border-b-4 border-orange-400 text-orange-400 cursor-pointer">
+              Next
+            </span>
+          }
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel={
+            <span className="px-4 py-2 rounded-lg btn btn-outline border-0 border-b-4 border-orange-400 text-orange-400 cursor-pointer">
+              Previous
+            </span>
+          }
+          renderOnZeroPageCount={null}
+          pageClassName="pagination-item"
+          className="flex gap-4 items-center"
+        />
+      </div>
     </div>
   );
 };
