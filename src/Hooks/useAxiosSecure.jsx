e6c29749c78b7b4a536d2e95e1c9270c3c0967b4
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-17 17:42:52 +0600

    user logged out is dont have valid token

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,15 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 export const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOutUser } = useAuth();
+
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
@@ -23,9 +28,14 @@ const useAxiosSecure = () => {
     function (response) {
       return response;
     },
-    function (error) {
+    async (error) => {
       const status = error.response.status;
       console.log("status error in the interceptor", status);
+      // for 401 and 403 logout the user and move the user to the login page
+      if (status === 401 || status === 403) {
+        await logOutUser();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
