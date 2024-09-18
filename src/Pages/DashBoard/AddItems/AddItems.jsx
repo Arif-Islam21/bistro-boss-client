e01e5cf1e4a180c946fe0b55c7a569f6a6cdf9ac
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-18 10:44:13 +0600

    working with add item page

--- src/Pages/DashBoard/AddItems/AddItems.jsx
+++ src/Pages/DashBoard/AddItems/AddItems.jsx
@@ -1,7 +1,90 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa6";
+
 const AddItems = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
-      <h2 className="text-4xl">Add Item page</h2>
+      <div>
+        <SectionTitle
+          heading={"add an item"}
+          subHeading={"What's New?"}
+        ></SectionTitle>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="form-control w-full my-6">
+            <div className="label">
+              <span className="label-text">Recipe Name *</span>
+            </div>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              placeholder="Recipe Name"
+              className="input input-bordered w-full "
+            />
+          </label>
+          <div className="flex gap-6">
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Price *</span>
+              </div>
+              <input
+                {...register("price")}
+                type="text"
+                placeholder="Price"
+                className="input input-bordered w-full "
+              />
+            </label>
+            <label className="form-control w-full">
+              <div className="label">
+                <span className="label-text">Category *</span>
+              </div>
+              <select
+                {...register("category")}
+                className="select select-bordered w-full "
+              >
+                <option disabled selected>
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">pizza</option>
+                <option value="soup">soup</option>
+                <option value="dessert">dessert</option>
+                <option value="drinks">drinks</option>
+              </select>
+            </label>
+          </div>
+          <label className="form-control">
+            <div className="label">
+              <span className="label-text">Recipe Details</span>
+            </div>
+            <textarea
+              {...register("recipe")}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </label>
+          <input
+            type="file"
+            {...register("image")}
+            className="file-input w-full my-6"
+          />
+
+          <button className="btn bg-orange-400 text-white">
+            Add Item <FaUtensils />
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
