03a99a9d3841511c1cf277076753fa24a50fc3f5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-09-18 23:35:03 +0600

    working with card payment

--- src/Pages/DashBoard/Payment/CheckOutForm.jsx
+++ src/Pages/DashBoard/Payment/CheckOutForm.jsx
@@ -1,8 +1,28 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import useCart from "../../../Hooks/useCart";
 
 const CheckOutForm = () => {
+  const [error, setError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+
   const stripe = useStripe();
   const elements = useElements();
+  const axiosSecure = useAxiosSecure();
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  useEffect(() => {
+    axiosSecure
+      .post("/create-payment-intent", {
+        price: totalPrice,
+      })
+      .then((res) => {
+        setClientSecret(res.data.clientSecret);
+        console.log(res.data.clientSecret);
+      });
+  }, [axiosSecure, totalPrice]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -22,8 +42,10 @@ const CheckOutForm = () => {
 
     if (error) {
       console.log("payment error", error);
+      setError(error.message);
     } else {
       console.log("payment method", paymentMethod);
+      setError("");
     }
   };
 
@@ -48,10 +70,11 @@ const CheckOutForm = () => {
       <button
         className="btn btn-outline px-6 bg-orange-400 border-none my-4 text-white font-semibold"
         type="submit"
-        disabled={!stripe}
+        disabled={!stripe || !clientSecret}
       >
         Pay
       </button>
+      <p className="text-red-400">{error}</p>
     </form>
   );
 };
