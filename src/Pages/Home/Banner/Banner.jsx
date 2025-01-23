a98d902d065ca5b5cbbb2b15a37129df4ea8321d
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-01-24 02:33:56 +0600

    banner section updated

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,34 +1,48 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
-import img1 from "../../../assets/home/01.jpg";
-import img2 from "../../../assets/home/02.jpg";
-import img3 from "../../../assets/home/03.png";
-import img4 from "../../../assets/home/04.jpg";
-import img5 from "../../../assets/home/05.png";
-import img6 from "../../../assets/home/06.png";
+import img1 from "../../../assets/home/7.avif";
+import img2 from "../../../assets/home/2.avif";
+import img3 from "../../../assets/home/3.avif";
+import img4 from "../../../assets/home/4.avif";
+import img5 from "../../../assets/home/5.avif";
+import img6 from "../../../assets/home/6.avif";
 
 const Banner = () => {
+  const bannerData = [
+    {
+      img: img1,
+      alt: "Image 1",
+    },
+    {
+      img: img2,
+      alt: "Image 2",
+    },
+    {
+      img: img3,
+      alt: "Image 3",
+    },
+    {
+      img: img4,
+      alt: "Image 4",
+    },
+    {
+      img: img5,
+      alt: "Image 5",
+    },
+    {
+      img: img6,
+      alt: "Image 6",
+    },
+  ];
+
   return (
     <Carousel>
-      <div>
-        <img src={img1} />
-      </div>
-      <div>
-        <img src={img2} />
-      </div>
-      <div>
-        <img src={img3} />
-      </div>
-      <div>
-        <img src={img4} />
-      </div>
-      <div>
-        <img src={img5} />
-      </div>
-      <div>
-        <img src={img6} />
-      </div>
+      {bannerData.map((item, index) => (
+        <div key={index}>
+          <img className="max-h-[95vh] w-full" src={item.img} alt={item.alt} />
+        </div>
+      ))}
     </Carousel>
   );
 };
